30440d5b39f79889da25d683366d6ce8552587d9
ianyoavvargasmontes-dotcom <ianyoav.vargasmontes@aulavirtual.umar.mx>
2026-01-26 08:49:07 -0600

    actu

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ async function fetchWeather() {
             [latitude, longitude] = cityInput.split(",");
         }
 
-        const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max&timezone=auto`;
+        const url = `https://open-meteo.com/en/docs?hourly=temperature_2m,rain,precipitation,precipitation_probability,apparent_temperature,dew_point_2m,relative_humidity_2m,showers,snowfall,snow_depth,weather_code,pressure_msl,surface_pressure,cloud_cover,cloud_cover_low,cloud_cover_mid,cloud_cover_high,visibility,evapotranspiration,et0_fao_evapotranspiration,vapour_pressure_deficit,wind_speed_10m,wind_speed_80m,wind_speed_120m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_120m,wind_direction_180m,wind_gusts_10m,temperature_80m,temperature_120m,temperature_180m,soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm,soil_temperature_54cm,soil_moisture_0_to_1cm,soil_moisture_1_to_3cm,soil_moisture_3_to_9cm,soil_moisture_9_to_27cm,soil_moisture_27_to_81cm&latitude=-11.4087&longitude=-69.3032`;
 
         const response = await fetch(url);
         if (!response.ok) throw new Error("Error al obtener datos");
